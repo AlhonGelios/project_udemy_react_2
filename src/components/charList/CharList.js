335d8ab3ccbbc38da21f40929563154562fecd62
Andrey <geliosal39hon@gmail.com>
2022-01-12 11:03:54 +0300

    lession 118 (1.1)

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -12,7 +12,7 @@ class CharList extends Component {
         loading: true,
         error: false,
         newItemLoading: false,
-        offset: 1539,
+        offset: 210,
         charEnded: false
     }
 
@@ -37,7 +37,7 @@ class CharList extends Component {
 
     onCharLoaded = (newChar) => {
         let ended = false
-        if (newChar < 9) {
+        if (newChar.length < 9) {
             ended = true
         }
 
