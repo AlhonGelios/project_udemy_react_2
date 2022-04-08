8c4f885350ab059d6f3d436add41d30cd74e8d12
Andrey <geliosal39hon@gmail.com>
2022-04-08 20:49:15 +0300

    lession 148

--- src/components/pages/singlePagaCharLayout/SingleCharacterPage.js
+++ src/components/pages/singlePagaCharLayout/SingleCharacterPage.js
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { Helmet } from 'react-helmet'
 
 import './singleComicPage.scss';
 
@@ -7,6 +8,13 @@ const SingleCharacterPage = ({data}) => {
 
     return (
         <div className='single-comic'>
+            <Helmet>
+                <meta
+                    name="description"
+                    content={`${name} character page`}
+                    />
+                <title>{name}</title>
+            </Helmet>
             <img src={thumbnail} alt={name} className="single-comic__img"/>
             <div className="single-comic__info">
                 <h2 className="single-comic__name">{name}</h2>
