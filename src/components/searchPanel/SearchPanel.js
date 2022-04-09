1bd1bc1b2962c34130cbe51b09c1e5ba356a84d8
Andrey <geliosal39hon@gmail.com>
2022-04-09 14:55:09 +0300

    lession 149(dz)

--- src/components/searchPanel/SearchPanel.js
+++ src/components/searchPanel/SearchPanel.js
@@ -5,34 +5,40 @@ import { Formik, Form, Field, ErrorMessage as FormikErrorMessage } from 'formik'
 import * as Yup from "yup";
 
 import ErrorMessage from '../errorMessage/ErrorMessage';
+import Spinner from '../spinner/Spinner';
 
 import useMarvelService from '../../services/MarvelService';
 
 import './searchPanel.scss'
 
+const setContent = (process, Component, data) => {
+    switch(process) {
+        case 'waiting':
+            return null
+        case 'loading':
+            return <Spinner/>
+        case 'confirmed':
+            return <Component data={data}/>
+        case 'error':
+            return <ErrorMessage/>
+        default:
+            throw new Error('Unexpected process state')
+    }
+}
+
 const SearchPanel = () => {
 
     const [char, setChar] = useState(null)
-    const {loading, error, getCharacterByName, clearError} = useMarvelService()
+    const {getCharacterByName, clearError, process, setProcess} = useMarvelService()
 
     const updateChar = (name) => {
         clearError()
-
-        getCharacterByName(name).then(res => setChar(res))
+        getCharacterByName(name)
+            .then(res => setChar(res))
+            .then(() => setProcess('confirmed'))
     }
 
-    const errorResult = error ? <div className="error">{<ErrorMessage/>}</div>: null
-
-    const result = !char ? null : char.length > 0 ?
-        <div className='char__search-response'>
-            <div className="error" style={{color: 'green'}}>There is! Visit {char[0].name} page?</div>
-            <Link to={`/character/${char[0].id}`} className = "button button__secondary">
-                <div className="inner">To page</div>
-            </Link>
-        </div> :
-        <div className="error">
-            The character was not found. Check the name and try again
-        </div>
+    const btnDisabled = process === 'loading' ? true : false
 
     return (
         <Formik
@@ -47,14 +53,34 @@ const SearchPanel = () => {
             <Form className="char__search">
                 <div className="char__search-title">Or find a character by name:</div>
                 <Field name='name' className='char__search-input' type="text" placeholder="Enter name"/>
-                <button type="submit" disabled={loading} className="button button__main">
+                <button type="submit" disabled={btnDisabled} className="button button__main">
                     <div className="inner">Find</div>
                 </button>
                 <FormikErrorMessage className="error" name="name" component="div"/>
-                {errorResult}
-                {result}
+                {/* {errorResult}
+                {result} */}
+                {setContent(process, Response, char)}
             </Form>
         </Formik>
+    )
+}
+
+const Response = ({data}) => {
+    return (
+        <>
+            {
+                !data ? null : data.length > 0 ?
+                <div className='char__search-response'>
+                    <div className="error" style={{color: 'green'}}>There is! Visit {data[0].name} page?</div>
+                    <Link to={`/character/${data[0].id}`} className = "button button__secondary">
+                        <div className="inner">To page</div>
+                    </Link>
+                </div> :
+                <div className="error">
+                    The character was not found. Check the name and try again
+                </div>
+            }
+        </>
     )
 }
 
